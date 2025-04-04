a668ebda6ac1e59653fa8f548d88a9a773a97270
Damir Paripovic <50810951+damirparipovic@users.noreply.github.com>
2025-04-04 14:28:33 -0700

    added additional req

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+function getRandomColour() {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function createBox(boxId, boxWidth, boxHeight) {
+    let newBox = document.createElement('div');
+    newBox.classList.add('box-item')
+    newBox.style.height = `${boxHeight}px`;
+    newBox.style.width = `${boxWidth}px`;
+    newBox.style.backgroundColor = 'white';
+    newBox.style.opacity = 1;
+    newBox.addEventListener('mouseover', (event) => {
+        let target = event.target;
+        // check for current color
+        // if has a color, make it darker
+        // if not color, get random color and make it a certain darkness
+        if (target.style.backgroundColor === 'white') {
+            target.style.backgroundColor = getRandomColour();
+        } else {
+            target.style.opacity -= 0.1;
+        }
+    })
+    newBox.id = boxId;
+
+    return newBox;
+}
+
 function createGrid(rows, cols) {
     const gridContainer = document.querySelector(".grid_container");
 
+    // clear old container of items
     gridContainer.innerHTML = '';
 
     const containerWidth = gridContainer.clientWidth;
@@ -12,15 +44,7 @@ function createGrid(rows, cols) {
     // create rows * cols boxes
     for (let bid = 0; bid < (rows * cols); bid++) {
 
-        let newBox = document.createElement('div');
-        newBox.classList.add('box-item')
-        newBox.style.height = `${boxHeight}px`;
-        newBox.style.width = `${boxWidth}px`;
-        newBox.addEventListener('mouseover', (event) => {
-            let target = event.target;
-            target.style.backgroundColor = 'pink';
-        })
-        newBox.id = bid;
+        let newBox = createBox(bid, boxWidth, boxHeight);
 
         gridContainer.appendChild(newBox);
     }
